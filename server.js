7e154b79de55244e8e6d29028f9d96ed4eb0999c
sgigli <simon.luciano@gigli.net>
2021-08-08 14:08:33 -0400

    configure cors

--- server.js
+++ server.js
@@ -3,18 +3,22 @@ const mongoose = require('mongoose')
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const passport = require('passport');
+const cors = require('cors');
 
 const User = require('./models/user.js');
 
 const app = express();
 
+const clientDevPort = 3000;
+const serverDevPort = 8080
+
+app.use(cors({ origin: process.env.CLIENT_ORIGIN || `http://localhost:${clientDevPort}` }))
 app.use(session({
   secret: 'r8q,+&1LM3)CD*zAGpx1xm{NeQhc;#',
   resave: false,
   saveUninitialized: true,
   cookie: { maxAge: 60 * 60 * 1000 }
 }));
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json())
 app.use(passport.initialize());
@@ -32,5 +36,5 @@ mongoose.connect('mongodb://localhost/anagram_app',{
   useUnifiedTopology: true
 });
 
-const port = 8080;
+const port = process.env.PORT || serverDevPort
 app.listen(port, () => console.log(`This app is listening on port ${port}`));
